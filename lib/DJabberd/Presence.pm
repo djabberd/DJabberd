4e8c6cf79055320faef09c52c598c8311a5dcacc
Brad Fitzpatrick <brad@danga.com>
2006-04-11 21:16:47 +0000

    clean up the handling of all the presence stuff.
    much more hackable/readable now.
    
    
    
    
    git-svn-id: http://code.sixapart.com/svn/djabberd/trunk@82 43dd9337-660f-0410-bd32-e7601923a1a1

--- lib/DJabberd/Presence.pm
+++ lib/DJabberd/Presence.pm
@@ -15,81 +15,117 @@ sub probe {
     return $class->downbless($xml);
 }
 
+sub type {
+    my $self = shift;
+    return
+        $self->attr("{jabber:client}type") ||
+        $self->attr("{jabber:server}type");
+
+}
+
+sub to_jid {
+    my $self = shift;
+    my $to =
+        $self->attr("{jabber:client}to") ||
+        $self->attr("{jabber:server}to")
+        or return undef;
+    return DJabberd::JID->new($to) or return undef;
+}
+
+sub from_jid {
+    my $self = shift;
+    my $from =
+        $self->attr("{jabber:client}from") ||
+        $self->attr("{jabber:server}from")
+        or return undef;
+    return DJabberd::JID->new($from) or return undef;
+}
+
+sub fail {
+    my ($self, $conn, $reason) = @_;
+    # TODO: figure this out (presence type='error' stuff, when?)
+    warn "PRESENCE FAILURE: $reason\n";
+    return;
+}
+
 sub process {
     confess "No generic 'process' method for $_[0]";
 }
 
-sub process_inbound {
+sub process_outbound {
     my ($self, $conn) = @_;
+    my $type      = $self->type || "available";
+    warn "OUTBOUND presence from $conn, type = '$type'\n";
 
-    my $fail = sub {
-        my $reason = shift;
-        warn "PRESENCE INBOUND FAILURE: $reason\n";
-        return;
-    };
+    return $self->fail($conn, "bogus type") unless $type =~ /^\w+$/;
 
-    my $is_client = $conn->isa("DJabberd::Connection::ClientIn");
-    my $ns        = $is_client ? "{jabber:client}" : "{jabber:server}";
-    my $type      = $self->attr("${ns}type");
-    my $to        = $self->attr("${ns}to");
-    my $to_jid    = DJabberd::JID->new($to)
-        or return $fail->("no/invalid 'to' attribute");
-    my $from      = $self->attr("${ns}from");
-    my $from_jid  = DJabberd::JID->new($from)
-        or return $fail->("no/invalid 'from' attribute");
-
-    if (!$type || $type eq "unavailable") {
-        $self->deliver($conn);
-        return;
+    my $meth = "_process_outbound_$type";
+    eval { $self->$meth($conn) };
+    if ($@) {
+        warn "  ... NOT IMPLEMENTED.\n";
     }
-
-    warn "INBOUND presence from $conn, type = '$type'\n";
-
-    if ($type eq "subscribe" || $type eq "subscribed" || $type eq "probe") {
-        # load the rosteritem in question before we decide whether to ignore/act
-        $conn->run_hook_chain(phase => "RosterLoadItem",
-                              args  => [ $to_jid, $from_jid ],
-                              methods => {
-                                  error   => sub {
-                                      my ($cb, $reason) = @_;
-                                      return $fail->("RosterLoadItem hook failed: $reason");
-                                  },
-                                  set => sub {
-                                      my ($cb, $ritem) = @_;
-
-                                      if ($type eq "subscribed") {
-                                          if ($ritem && $ritem->subscription->pending_out) {
-                                              $self->_process_subscribed($conn, $to_jid, $ritem);
-                                          }
-                                      } elsif ($type eq "subscribe") {
-                                          # ...  vivivy to whatever+pending in in roster
-
-                                          $self->deliver($conn);  # TEMP
-                                      } elsif ($type eq "probe") {
-                                          warn("Got a PROBE from " . $from_jid->as_string . " and ritem = $ritem\n");
-                                          if ($ritem && $ritem->subscription->sub_from) {
-                                              # ....
-                                          }
-                                      }
-
-
-                                  },
-                              },
-                              fallback => sub {
-                                  return $fail->("no RosterLoadItem hooks");
-                              });
-        return;
-    }
-
-    warn "$self PRESENCE INBOUND type='$type' from $conn not implemented\n";
+    return;
 }
 
-# called after filtering.  at this point, we know $to_jid was waiting
-# for the 'subscribed' stanza
-sub _process_subscribed {
-    my ($self, $conn, $to_jid, $ritem) = @_;
+sub process_inbound {
+    my ($self, $conn) = @_;
+    my $type      = $self->type || "available";
+    warn "INBOUND presence from $conn, type = '$type'\n";
+    return $self->fail($conn, "bogus type") unless $type =~ /^\w+$/;
 
-    warn "processing subscribed...\n";
+    my $to_jid    = $self->to_jid
+        or return $self->fail($conn, "no/invalid 'to' attribute");
+    my $from_jid  = $self->from_jid
+        or return $self->fail($conn, "no/invalid 'from' attribute");
+
+    # find the RosterItem corresponding to this sender, and only once we have
+    # it, invoke the next handler
+    $conn->run_hook_chain(phase => "RosterLoadItem",
+                          args  => [ $to_jid, $from_jid ],
+                          methods => {
+                              error   => sub {
+                                  my ($cb, $reason) = @_;
+                                  return $self->fail($conn, "RosterLoadItem hook failed: $reason");
+                              },
+                              set => sub {
+                                  my ($cb, $ritem) = @_;
+
+                                  my $meth = "_process_inbound_$type";
+                                  eval { $self->$meth($conn, $ritem) };
+                                  if ($@) {
+                                      warn "  ... NOT IMPLEMENTED.\n";
+                                  }
+                              },
+                          });
+}
+
+sub _process_inbound_available {
+    my ($self, $conn, $ritem) = @_;
+    $self->deliver($conn);
+}
+
+sub _process_inbound_unavailable {
+    my ($self, $conn, $ritem) = @_;
+    $self->deliver($conn);
+}
+
+sub _process_inbound_subscribe {
+    my ($self, $conn, $ritem) = @_;
+
+    # ...  vivivy to whatever+pending in in roster
+    $self->deliver($conn);  # TEMP
+}
+
+sub _process_inbound_subscribed {
+    my ($self, $conn, $ritem) = @_;
+
+    # MUST ignore inbound subscribed if we weren't awaiting
+    # its arrival
+    return unless $ritem && $ritem->subscription->pending_out;
+
+    my $to_jid    = $self->to_jid;
+
+    warn "processing inbound subscribed...\n";
     $ritem->subscription->got_inbound_subscribed;
 
     $conn->run_hook_chain(phase => "RosterSetItem",
@@ -105,86 +141,83 @@ sub _process_subscribed {
 
 }
 
-sub process_outbound {
+sub _process_inbound_probe {
+    my ($self, $conn, $ritem) = @_;
+
+    warn("Got a PROBE from " . $ritem->jid->as_string . " and ritem = $ritem\n");
+    if ($ritem && $ritem->subscription->sub_from) {
+        # ....
+    }
+}
+
+
+sub _process_outbound_available {
+    my ($self, $conn) = @_;
+    if ($conn->is_initial_presence) {
+        $conn->send_presence_probes;
+    }
+    warn "NOT IMPLEMENTED: Outbound presence broadcast!\n";
+}
+
+sub _process_outbound_unavailable {
+    my ($self, $conn) = @_;
+    warn "NOT IMPLEMENTED: Unavailable presence broadcast!\n";
+}
+
+sub _process_outbound_subscribe {
     my ($self, $conn) = @_;
 
-    my $ns        = "{jabber:client}";
-    my $type      = $self->attr("${ns}type");
-    my $to        = $self->attr("${ns}to");
+    my $contact_jid = $self->to_jid or die "Can't subscribe to bogus jid";
 
-    my $fail = sub {
-        my $reason = shift;
-        warn "PRESENCE FAILURE: $reason\n";
-        return;
-    };
-
-    warn "OUTBOUND presence from $conn, type = '$type'\n";
-
-    if (! $type || $type eq "unavailable") {
-        if ($conn->is_initial_presence) {
-            $conn->send_presence_probes;
-        }
-        warn "NOT IMPLEMENTED: Outbound presence broadcast!\n";
-        return;
-    }
-
-    if ($type eq "subscribe") {
-        my $contact_jid = DJabberd::JID->new($to)
-            or return $fail->("no/invalid 'to' attribute");
-
-        $conn->run_hook_chain(phase => "RosterSubscribe",
-                              args  => [ $contact_jid ],
-                              methods => {
-                                  error   => sub {
-                                      my ($cb, $reason) = @_;
-                                      return $fail->("RosterSubscribe hook failed: $reason");
-                                  },
-                                  done => sub {
-                                      # XMPPIP-9.2-p2: MUST without exception
-                                      # route these, to combat sync issues
-                                      # between parties
-                                      if ($conn->vhost->handles_jid($contact_jid)) {
-                                          my $clone = $self->clone;
-                                          $clone->process_inbound($conn);
-                                      } else {
-                                          $self->deliver($conn);
-                                      }
-                                  },
+    $conn->run_hook_chain(phase => "RosterSubscribe",
+                          args  => [ $contact_jid ],
+                          methods => {
+                              error   => sub {
+                                  my ($cb, $reason) = @_;
+                                  return $self->fail($conn, "RosterSubscribe hook failed: $reason");
                               },
-                              fallback => sub {
-                                  return $fail->("no RosterSubscribe hooks");
-                              });
+                              done => sub {
+                                  # XMPPIP-9.2-p2: MUST without exception
+                                  # route these, to combat sync issues
+                                  # between parties
+                                  if ($conn->vhost->handles_jid($contact_jid)) {
+                                      my $clone = $self->clone;
+                                      $clone->process_inbound($conn);
+                                  } else {
+                                      warn "Delivering subscribe request...\n";
+                                      $self->deliver($conn);
+                                  }
+                              },
+                          },
+                          fallback => sub {
+                              return $self->fail($conn, "no RosterSubscribe hooks");
+                          });
+}
 
-        return;
-    }
+sub _process_outbound_subscribed {
+    my ($self, $conn) = @_;
 
     # user wanting to subscribe or approve subscription request to contact
-    if ($type eq "subscribed") {
-        my $contact_jid = DJabberd::JID->new($to)
-            or return $fail->("no/invalid 'to' attribute");
+    my $contact_jid = $self->to_jid
+        or return $self->fail($conn, "no/invalid 'to' attribute");
 
-        $conn->run_hook_chain(phase => "RosterSubscribed",
-                              args  => [ $contact_jid ],
-                              methods => {
-                                  error   => sub {
-                                      my ($cb, $reason) = @_;
-                                      return $fail->("RosterSubscribe hook failed: $reason");
-                                  },
-                                  done => sub {
-
-                                      # TODO: based on $type, set things in roster
-                                      # now send the packet to the other party
-                                      $self->deliver($conn);
-                                  },
+    $conn->run_hook_chain(phase => "RosterSubscribed",
+                          args  => [ $contact_jid ],
+                          methods => {
+                              error   => sub {
+                                  my ($cb, $reason) = @_;
+                                  return $self->fail($conn, "RosterSubscribe hook failed: $reason");
                               },
-                              fallback => sub {
-                                  return $fail->("no RosterSubscribe hooks");
-                              });
+                              done => sub {
 
-        return;
-    }
-
-    warn "$self PRESENCE OUTBOUND type='$type' from $conn not implemented\n";
+                                  # TODO: based on $type, set things in roster
+                                  # now send the packet to the other party
+                                  $self->deliver($conn);
+                              },
+                          },
+                          fallback => sub {
+                              return $self->fail($conn, "no RosterSubscribe hooks");
+                          });
 }
 
 
