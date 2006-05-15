9f0d26c2d2f5369370a29ddc72b2f4ea1ee641c7
Artur Bergman <abergman@cpan.org>
2006-05-15 07:09:48 +0000

    initial vcard support, uses sqlite, needs refactoring as per notes in VCard.pm
    git-svn-id: http://code.sixapart.com/svn/djabberd/trunk@280 43dd9337-660f-0410-bd32-e7601923a1a1

---
+++ lib/DJabberd/Plugin/VCard.pm
@@ -0,0 +1,165 @@
+package DJabberd::Plugin::VCard;
+use strict;
+use base 'DJabberd::Plugin';
+use warnings;
+use DBI;
+
+our $logger = DJabberd::Log->get_logger();
+
+# this spec implements the jep--0054 vcard spec using an SQLite store
+# later it should be refactored to have pluggable stores
+# but I really just want to get it out there -- sky
+# This should be split into the following
+#  DJabberd::Plugin::VCard::IQ isa DJabberd::IQ
+#  DJabberd::Plugin::VCard::SQLite isa Djabberd::Plugin::VCard
+#  DJabberd::Plugin::VCard manages and loads everythign
+
+
+
+our %vcards;
+
+sub set_config_storage {
+    my ($self, $dbfile) = @_;
+    $self->{dbfile} = $dbfile;
+}
+
+
+sub finalize {
+    my $self = shift;
+
+    $logger->error_die("No 'Database' configured'") unless $self->{dbfile};
+
+    my $dbh = DBI->connect("dbi:SQLite:dbname=$self->{dbfile}","","", { RaiseError => 1, PrintError => 0, AutoCommit => 1 });
+    $self->{dbh} = $dbh;
+    $self->check_install_schema;
+    $logger->info("Loaded SQLite VCArd using file '$self->{dbfile}'");
+
+}
+
+sub register {
+    my ($self, $vhost) = @_;
+    my $cb = sub {
+        my ($vh, $cb, $iq) = @_;
+        unless ($iq->isa("DJabberd::IQ")) {
+            $cb->decline;
+            return;
+        }
+        if(my $to = $iq->to_jid) {
+            unless ($vhost->handles_jid($to)) {
+                $cb->decline;
+                return;
+            }
+        }
+        if ($iq->signature eq 'get-{vcard-temp}vCard') {
+            $self->get_vcard($vh, $iq);
+            $cb->stop_chain;
+            return;
+        } elsif ($iq->signature eq 'set-{vcard-temp}vCard') {
+            $self->set_vcard($vh, $iq);
+            $cb->stop_chain;
+            return;
+        }
+        $cb->decline;
+    };
+    $vhost->register_hook("switch_incoming_client",$cb);
+    $vhost->register_hook("switch_incoming_server",$cb);
+
+
+}
+
+
+sub get_vcard {
+    my ($self, $vhost, $iq) = @_;
+
+
+    my $user;
+    if ($iq->to) {
+        $user = $iq->to_jid->as_bare_string;
+    } else {
+        # user is requesting their own vCard
+        $user = $iq->connection->bound_jid->as_bare_string;
+    }
+    $logger->info("Getting vcard for user '$user'");
+    my $reply;
+    if(my $vcard = $self->load_vcard($user)) {
+        $reply = $self->make_response($iq, $vcard);
+    } else {
+        $reply = $self->make_response($iq, "<vCard xmlns='vcard-temp'/>");
+    }
+    $reply->deliver($vhost);
+
+}
+
+
+sub make_response {
+    my ($self, $iq, $vcard) = @_;
+
+
+
+    my $response = $iq->clone;
+    my $from = $iq->from;
+    my $to   = $iq->to;
+    $response->set_raw($vcard);
+
+    $response->attrs->{"{}type"} = 'result';
+    $response->set_to($from);
+    $to ? $response->set_from($to) : delete($response->attrs->{"{}from"});
+
+#    Test::More::diag $iq->as_xml;
+#    Test::More::diag $response->as_xml;
+    return $response;
+}
+
+sub set_vcard {
+    my ($self, $vhost, $iq) = @_;
+
+    my $vcard = $iq->first_element();
+    my $user  = $iq->connection->bound_jid->as_bare_string;
+    $self->store_vcard($user, $vcard);
+
+    $logger->info("Set vcard for user '$user'");
+    $self->make_response($iq)->deliver($vhost);
+}
+
+sub load_vcard {
+    my ($self, $user) = @_;
+
+    my $vcard = $self->{dbh}->selectrow_hashref("SELECT * FROM vcard WHERE jid=?",
+                                         undef, $user);
+
+
+    return unless $vcard;
+    return $vcard->{vcard};
+}
+
+sub store_vcard {
+    my ($self, $user, $vcard) = @_;
+
+    my $exists = $self->load_vcard($user);
+
+    if($exists) {
+        $self->{dbh}->do("UPDATE vcard SET vcard = ? WHERE jid = ?", undef, $vcard->as_xml, $user);
+    } else {
+        $self->{dbh}->do("INSERT INTO vcard (vcard, jid) VALUES (?,?)", undef, $vcard->as_xml, $user);
+    }
+
+}
+
+sub check_install_schema {
+    my $self = shift;
+    my $dbh = $self->{dbh};
+
+    eval {
+        $dbh->do(qq{
+            CREATE TABLE vcard (
+                                 jid   VARCHAR(255) NOT NULL,
+                                 vcard TEXT,
+                                 UNIQUE (jid)
+                                 )})
+        };
+    if ($@ && $@ !~ /table \w+ already exists/) {
+        $logger->logdie("SQL error $@");
+    }
+
+}
+1;
---
+++ t/vcard.t
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+
+
+use strict;
+use Test::More tests => 9;
+use lib 't/lib';
+#BEGIN {  $ENV{LOGLEVEL} ||= "FATAL" }
+BEGIN { require 'djabberd-test.pl' }
+
+
+
+use_ok("DJabberd::Plugin::VCard");
+
+
+$Test::DJabberd::Server::PLUGIN_CB = sub {
+    my $self = shift;
+    my $plugins = $self->standard_plugins();
+    push @$plugins, DJabberd::Plugin::VCard->new(storage => $self->roster_name);
+    return $plugins;
+};
+
+#really there should be a way to set plugins before this runs so I could just use once_logged_in
+two_parties( sub {
+    my ($pa, $pb) = @_;
+    $pa->login;
+    $pb->login;
+
+    $pa->send_xml("<iq
+    from='$pa/testsuite'
+    type='get'
+    id='v1'>
+  <vCard xmlns='vcard-temp'/>
+</iq>");
+
+
+    my $xml = $pa->recv_xml;
+
+
+    like($xml, qr{<vCard xmlns='vcard-temp'/>}, "No vcard set, getting empty vcard back");
+    $pa->send_xml("<iq
+    from='$pa/testsuite'
+    type='set'
+    id='v2'>
+  <vCard xmlns='vcard-temp'><FN>Test User</FN></vCard>
+</iq>");
+
+    $xml = $pa->recv_xml;
+    like($xml, qr/result/, "Got a result back on the set");
+
+
+    $pa->send_xml("<iq
+    from='$pa/testsuite'
+    type='get'
+    id='v3'>
+  <vCard xmlns='vcard-temp'/>
+</iq>");
+
+    $xml = $pa->recv_xml;
+    like($xml, qr{Test User}, "Got the vcard back");
+
+
+
+    $pb->send_xml("<iq
+    from='$pb/testsuite'
+    to='$pa'
+    type='get'
+    id='v1'>
+  <vCard xmlns='vcard-temp'/>
+</iq>");
+
+    $xml = $pb->recv_xml;
+    like($xml, qr{Test User}, "Got the vcard back to user pb from pa");
+});
+
+
