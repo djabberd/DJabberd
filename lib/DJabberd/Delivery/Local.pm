c2041aa753873ed5aedb9f83e3c4b6a36893034a
Brad Fitzpatrick <brad@danga.com>
2006-06-15 05:35:53 +0000

    only deliver to available resources
    git-svn-id: http://code.sixapart.com/svn/djabberd/trunk@356 43dd9337-660f-0410-bd32-e7601923a1a1

--- lib/DJabberd/Delivery/Local.pm
+++ lib/DJabberd/Delivery/Local.pm
@@ -10,13 +10,22 @@ sub deliver {
     my $to = $stanza->to_jid                or return $cb->declined;
 
     my @dconns;
+    my $find_bares = sub {
+        @dconns = grep { $_->is_available } $vhost->find_conns_of_bare($to)
+    };
+
     if ($to->is_bare) {
-        @dconns = $vhost->find_conns_of_bare($to)
-            or return $cb->declined;
+        $find_bares->();
     } else {
-        $dconns[0] = $vhost->find_jid($to)
-            or return $cb->declined;
+        my $dest;
+        if (($dest = $vhost->find_jid($to)) && $dest->is_available) {
+            push @dconns, $dest;
+        } else {
+            $find_bares->();
+        }
     }
+
+    return $cb->declined unless @dconns;
 
     foreach my $c (@dconns) {
         $c->send_stanza($stanza);
