5e77223d06be43dd3b018ede3ed8c5852a1253f2
Brad Fitzpatrick <brad@danga.com>
2006-06-29 22:25:55 +0000

    update LJ roster plugin, at least the core of it
    git-svn-id: http://code.sixapart.com/svn/djabberd/trunk@455 43dd9337-660f-0410-bd32-e7601923a1a1

--- lib/DJabberd/RosterStorage/LiveJournal.pm
+++ lib/DJabberd/RosterStorage/LiveJournal.pm
@@ -12,16 +12,30 @@ sub get_roster {
     my $roster = DJabberd::Roster->new;
 
     my $friends = LWP::Simple::get("http://www.livejournal.com/misc/fdata.bml?user=" . $user);
+    my %relation;  # user -> to (1) / both (2)
+
+    # we depend on all the ">" to edges before the "<" from edges:
     foreach my $line (sort split(/\n/, $friends)) {
-        next unless $line =~ m!> (\w+)!;
-        my $fuser = $1;
+        if ($line =~ m!> (\w+)!) {
+            $relation{$1} = 1;  # to
+        } elsif ($line =~ m!< (\w+)! && $relation{$1} == 1) {
+            $relation{$1} = 2;  # both
+        }
+    }
+
+    foreach my $user (sort keys %relation) {
         my $ri = DJabberd::RosterItem->new(
-                                           jid => "$fuser\@" . $conn->vhost->name,
-                                           name => $fuser,
+                                           jid => "$user\@" . $conn->vhost->name,
+                                           name => $user,
                                            );
-        $ri->add_group("LJ Friends");
+        $ri->subscription->set_to;
+        if ($relation{$user} == 2) {
+            $ri->subscription->set_from;
+        }
+        $ri->add_group("LiveJournal");
         $roster->add($ri);
     }
+
     $cb->set_roster($roster);
 }
 
