5e6370712b565484c73736306a9703d829a32cbc
Brad Fitzpatrick <brad@danga.com>
2006-07-02 18:27:33 +0000

    let messages go to server.  it might be a bot.
    git-svn-id: http://code.sixapart.com/svn/djabberd/trunk@511 43dd9337-660f-0410-bd32-e7601923a1a1

--- lib/DJabberd/Message.pm
+++ lib/DJabberd/Message.pm
@@ -6,9 +6,8 @@ sub on_recv_from_client {
     my ($self, $conn) = @_;
 
     my $to = $self->to_jid;
-    if (! $to || $conn->vhost->uses_jid($to)) {
-        # FIXME: use proper stream error code.
-        warn "Can't process a message to the server.\n";
+    if (! $to) {
+        warn "Can't process a message to nobody.\n";
         $conn->close;
         return;
     }
