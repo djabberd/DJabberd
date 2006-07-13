2c56c1f682d023229a8787bbfd27c41745108741
Mart Atkins <mart@degeneration.co.uk>
2006-07-13 20:58:11 +0000

    Completely rewritten implementation of DJabberd::Component::Easier for a nicer API and less code duplication.
    Most of the fun stuff is now in DJabberd::Agent, which represents "thing to collect together methods that are used both by components and nodes". It's sick, but it was the best way I could think of to arrange these things to keep all of the common stanza processing stuff in one place.
    
    git-svn-id: http://code.sixapart.com/svn/djabberd/trunk@625 43dd9337-660f-0410-bd32-e7601923a1a1

---
+++ lib/DJabberd/Agent.pm
@@ -0,0 +1,137 @@
+
+# An abstract base class providing a higher-level API for
+# component/service implementation.
+# Subclasses can override different functions to override
+# behavior at a variety of levels.
+
+package DJabberd::Agent;
+use base qw(DJabberd::Component);
+use strict;
+use warnings;
+use DJabberd::Util qw(exml);
+use DJabberd::JID;
+
+sub handle_stanza {
+    my ($self, $vhost, $stanza) = @_;
+
+    return $self->handle_iq($vhost, $stanza) if $stanza->isa('DJabberd::IQ');
+    return $self->handle_message($vhost, $stanza) if $stanza->isa('DJabberd::Message');
+    return $self->handle_presence($vhost, $stanza) if $stanza->isa('DJabberd::Presence');    
+
+}
+
+sub name {
+    my ($self) = @_;
+    
+    return "";
+}
+
+sub handle_message {
+    my ($self, $vhost, $stanza) = @_;
+    
+    # By default, ignore the message altogether
+}
+
+sub handle_presence {
+    my ($self, $vhost, $stanza) = @_;
+    
+    # By default, ignore the presence altogether
+}
+
+sub handle_iq {
+    my ($self, $vhost, $stanza) = @_;
+    
+    my $sig = $stanza->signature;
+    
+    if ($self->{djabberd_agent_iqcb} && $self->{djabberd_agent_iqcb}{$sig}) {
+        return $self->{djabberd_agent_iqcb}{$sig}->($vhost, $stanza);
+    }
+
+    return $self->handle_iq_vcard($vhost,$stanza) if $sig eq 'get-{vcard-temp}vCard';
+    return $self->handle_iq_disco_info($vhost,$stanza) if $sig eq 'get-{http://jabber.org/protocol/disco#info}query';
+    return $self->handle_iq_disco_items($vhost,$stanza) if $sig eq 'get-{http://jabber.org/protocol/disco#items}query';
+    
+    # If we've got this far, then we don't support this IQ type
+    $stanza->make_error_response('501', 'cancel', 'feature-not-implemented')->deliver($vhost);
+}
+
+sub register_iq_handler {
+    my ($self, $signature, $handler) = @_;
+    
+    $self->{easier_node_iqcb} ||= {};
+    $self->{easier_node_iqcb}{$signature} = $handler;
+}
+
+sub handle_iq_vcard {
+    my ($self, $vhost, $stanza) = @_;
+    
+    my $vcard = $self->vcard($stanza->to_jid);
+
+    my $response = $stanza->make_response();
+    $response->set_raw("<vCard xmlns='vcard-temp'>".$vcard."</vCard>");
+    $response->deliver($vhost);    
+}
+
+sub handle_iq_disco_info {
+    my ($self, $vhost, $stanza) = @_;
+
+    my $features = $self->features($stanza->from_jid);
+    my $identities = $self->identities($stanza->from_jid);
+    
+    my $response = $stanza->make_response();
+
+    my $xml = "<query xmlns='http://jabber.org/protocol/disco#info'>" .
+              join('',map({ "<identity category='".exml($_->[0])."' type='".exml($_->[1])."' name='".exml($_->[2])."'/>" } @$identities)) .
+              join('',map({ "<feature var='".exml($_)."' />" } @$features)) .
+              "</query>";
+    $response->set_raw($xml);
+    $response->deliver($vhost);
+}
+
+sub handle_iq_disco_items {
+    my ($self, $vhost, $stanza) = @_;
+
+    my $items = $self->child_services($stanza->from_jid);
+    
+    my $response = $stanza->make_response();
+
+    my $xml = "<query xmlns='http://jabber.org/protocol/disco#items'>" .
+              join('',map({ "<item jid='".exml($_->[0])."' name='".exml($_->[1])."'/>" } @$items)) .
+              "</query>";
+    $response->set_raw($xml);
+    $response->deliver($vhost);
+}
+
+sub vcard {
+    my ($self, $requester_jid) = @_;
+
+    # Empty vCard by default
+    return "";
+}
+
+sub features {
+    my ($self, $requester_jid) = @_;
+    
+    # We support disco and vCards by default
+    return [
+        'vcard-temp',
+        'http://jabber.org/protocol/disco#info',
+        'http://jabber.org/protocol/disco#items',
+    ];
+}
+
+sub identities {
+    my ($self, $requester_jid) = @_;
+    
+    # No identities by default
+    return [];
+}
+
+sub child_services {
+    my ($self, $requester_jid) = @_;
+
+    # None by default
+    return [];
+}
+
+1;
