fb907ca4b5083c00b1857a2b1c434ed10c191c49
Bron Gondwana <brong@fastmail.fm>
2008-07-29 07:29:17 +0000

    Simple typo fix
    git-svn-id: http://code.sixapart.com/svn/djabberd/trunk@785 43dd9337-660f-0410-bd32-e7601923a1a1

--- lib/DJabberd/DialbackParams.pm
+++ lib/DJabberd/DialbackParams.pm
@@ -25,7 +25,7 @@ sub concat_params {
 sub generate_dialback_result {
     my ($self, $cb) = @_;
     my $vhost = $self->{vhost};
-    $logger->info("Generating diablack result for vhost $vhost->{server_name}");
+    $logger->info("Generating dialback result for vhost $vhost->{server_name}");
     $vhost->get_secret_key(sub {
         my ($shandle, $secret) = @_;
         my $params = $self->concat_params;
