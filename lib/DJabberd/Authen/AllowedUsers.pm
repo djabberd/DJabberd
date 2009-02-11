cd7deb041dfc715ba5f63049a6cec87e17232fa2
Yann Kerherve <yann@cyberion.net>
2009-02-11 21:57:08 +0000

    Finally got tired of this warning
    git-svn-id: http://code.sixapart.com/svn/djabberd/trunk@852 43dd9337-660f-0410-bd32-e7601923a1a1

--- lib/DJabberd/Authen/AllowedUsers.pm
+++ lib/DJabberd/Authen/AllowedUsers.pm
@@ -3,6 +3,10 @@ use strict;
 use base 'DJabberd::Authen';
 use Carp qw(croak);
 
+use DJabberd::Log;
+
+our $logger = DJabberd::Log->get_logger;
+
 sub set_config_policy {
     my ($self, $policy) = @_;
     $policy = lc $policy;
@@ -33,7 +37,7 @@ sub check_cleartext {
     my $user = $args{'username'};
 
     if ($self->{'policy'} eq "deny") {
-        warn "$self --- user=$user, denying, unless allowed: @{$self->{allowed}}\n";
+        $logger->debug("$self --- user=$user, denying, unless allowed: @{$self->{allowed}}");
         foreach my $allowed (@{$self->{allowed}}) {
             if (ref $allowed eq "Regexp" && $user =~ /$allowed/) {
                 $cb->decline; # okay username, may continue in auth phase
@@ -48,7 +52,7 @@ sub check_cleartext {
     }
 
     if ($self->{'policy'} eq "accept") {
-        warn "$self --- user=$user, accepting, unless denied: @{$self->{denied}}\n";
+        $logger->debug("$self --- user=$user, accepting, unless denied: @{$self->{denied}}");
         foreach my $allowed (@{$self->{denied}}) {
             if (ref $allowed eq "Regexp" && $user =~ /$allowed/) {
                 $cb->reject; # okay username, may continue in auth phase
